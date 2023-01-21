a287b11c572e015969f4966f38720f0918f3ab29
jgsch <jean.gschwind@protonmail.com>
2023-01-21 11:46:45 +0100

    feat(front): add deceased page

--- apps/front/src/components/layout/Sidebar.tsx
+++ apps/front/src/components/layout/Sidebar.tsx
@@ -4,12 +4,14 @@ import { List } from '@mui/material';
 import SidebarLink from './SidebarLink';
 import MapIcon from '@mui/icons-material/Map';
 import ChurchIcon from '@mui/icons-material/Church';
+import SentimentNeutralIcon from '@mui/icons-material/SentimentNeutral';
 
 const Sidebar: FC = () => {
   return (
     <List>
       <SidebarLink name="Dashboard" href="/" icon={<MapIcon />} />
       <SidebarLink name="Cemeteries" href="/cemeteries" icon={<ChurchIcon />} />
+      <SidebarLink name="Deceased" href="/deceased" icon={<SentimentNeutralIcon />} />
     </List>
   );
 };
